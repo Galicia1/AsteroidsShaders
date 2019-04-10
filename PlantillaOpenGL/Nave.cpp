de6facc4513944713a1b275ba4d2e4aa3e0ded45
Galicia <sakushiben@gmail.com>
2019-04-09 19:00:55 -0700

    Aceleracion, desaceleracion, teletransportaciom y otros

--- PlantillaOpenGL/Nave.cpp
+++ PlantillaOpenGL/Nave.cpp
@@ -37,6 +37,9 @@ void Nave::rotar(Direccion direccion) {
 }
 
 void Nave::avanzar() {
+	if(velocidad < velocidadMaxima){
+		velocidad += aceleracion * tiempoDiferencial;
+	}
 	float anguloDesfasado = angulo + 90.0f;
 	vec3 traslacion = vec3(
 		cos(anguloDesfasado*3.14159 / 180.0f)*velocidad,//X
@@ -48,10 +51,50 @@ void Nave::avanzar() {
 
 }
 
+
+void Nave::detenerse() {
+	velocidad -= desaceleracion * tiempoDiferencial;
+	if (velocidad < 0) {
+		velocidad = 0.0f;
+	}
+	float anguloDesfasado = angulo + 90.0f;
+	vec3 traslacion = vec3(
+		cos(anguloDesfasado * 3.14159 / 180.0f) * velocidad,//X
+		sin(anguloDesfasado * 3.14159 / 180.0f) * velocidad,//Y
+		0.0f
+	);
+	coordenadas += traslacion;
+	actualizarMatrizTransformacion();
+
+}
+
+
 void Nave::actualizarMatrizTransformacion() {
 	//Partir siempre de una matriz identidad
 	transformaciones = mat4(1.0f);
 	//Aplicar transformaciones}
 	transformaciones = translate(transformaciones, coordenadas);
 	transformaciones = rotate(transformaciones, angulo*3.14159f / 180.0f, vec3(0.0f, 0.0f, 0.1f));
+}
+
+void Nave::teletransportar() {
+	if (coordenadas.x < -1.1f)
+	{
+		coordenadas.x += 2;
+	}
+	 if (coordenadas.x > 1.1f)
+	{
+		coordenadas.x -= 2;
+	}
+
+	
+
+	if (coordenadas.y < -1.1f)
+	{
+		coordenadas.y += 2;
+	}
+	if (coordenadas.y > 1.1f)
+	{
+		coordenadas.y -= 2;
+	}
 }
--- PlantillaOpenGL/Nave.h
+++ PlantillaOpenGL/Nave.h
@@ -9,9 +9,22 @@ public:
 	vec3 coordenadas;
 	enum Direccion {Izquierda, Derecha};
 	float angulo;
-	float velocidadAngular = 0.5f;
-	float velocidad = 0.001f;
+
+	float velocidadAngular = 1.5f;
+	float velocidad = 0.0f;
+	float velocidadMaxima = 0.008f;
+
+
+	float aceleracion = 0.004f;
+	float desaceleracion = 0.002f;
+
 	void rotar(Direccion direccion);
 	void avanzar();
+	void detenerse();
 	void actualizarMatrizTransformacion();
+	void teletransportar();
+
+	double tiempoAnterior = 0.0f;
+	double tiempoActual = 0.0f;
+	double tiempoDiferencial = 0.0f;
 };
